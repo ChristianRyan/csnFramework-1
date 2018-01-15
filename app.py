5e0c661f933328a891b163649d55e96d884a89af
jankovicgd <jankovic.gd@gmail.com>
2018-01-15 13:28:07 +0100

    modified app for testing, added todos and report function

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import pandas as pd
 import numpy as np
+from functions.mainFunction import knn
+
+# This is the main module the user calls
+
+
+# The bulk of it goes below
+def main():
+    print(knn)
+    print(knn.get_params())
+    
+# TODO: tie the functions together
+
+# TODO: produce the report
+
+if __name__ == "__main__":
+    main()
--- functions/mainFunction.py
+++ functions/mainFunction.py
@@ -1,4 +1,10 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from report import *
+
 # TODO: create knn classifier
+knn = KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=-1)
 
 # TODO: optimize parameters
 
