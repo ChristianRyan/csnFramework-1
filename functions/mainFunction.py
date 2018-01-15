7560a2cbdc98d63ec3f8681153aca9a3efeb6150
jankovicgd <jankovic.gd@gmail.com>
2018-01-15 15:38:06 +0100

    more todos

--- functions/mainFunction.py
+++ functions/mainFunction.py
@@ -2,10 +2,17 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from report import *
+from importDataset import *
 
 # TODO: create knn classifier
 knn = KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=-1)
 
-# TODO: optimize parameters
+# TODO: 1 optimize parameters
+
+# TODO: 1.1 Grid search
+
+# TODO: 1.2 Randomized search
+
+# TODO: 1.3 OurImplementation for brute force arround sqrt(n)
 
 # TODO: export report
