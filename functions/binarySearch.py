4ca4bdeb675ccefba0de600e4b0af58bab0c1d7f
ChristianRyan <christian.sean.ryan@gmail.com>
2018-01-30 18:58:58 +0100

    Fixing Binary

--- functions/binarySearch.py
+++ functions/binarySearch.py
@@ -12,62 +12,69 @@ from sklearn.model_selection import cross_validate
 #from sklearn.cross_validation import cross_validate
 
 def binarySearch(dataset, classifier):
-    # Get the n
-    n = dataset.shape[0]
-    # Get the l
-    l = dataset.shape[1]
-    # lower/upperbounds
-    lb = 1
-    ub = round(2*n/3) - 1
+    returnListDfs = []
+    for idx, data in enumerate(dataset):
+        # Get the n
+        n = data.shape[0]
+        # Get the l
+        l = data.shape[1]
+        # lower/upperbounds
+        lb = 1
+        ub = round(2*n/3) - 1
 
-    y = dataset['target']
-    dataset = dataset.drop('target', axis=1)
-    #'recall_macro', 'recall_micro', 'precision_macro', 'precision_micro'
-    score_params = ['accuracy']
+        y = data['target']
+        X = data.drop('target', axis=1)
+        #'recall_macro', 'recall_micro', 'precision_macro', 'precision_micro'
+        score_params = ['accuracy']
 
-    classifier.set_params(n_neighbors = ub)
-    scores = cross_validate(classifier, dataset, y, scoring=score_params, return_train_score=False)
-    scores_ub = scores['test_accuracy'].mean()
-    classifier.set_params(n_neighbors = lb)
-    scores = cross_validate(classifier, dataset, y, scoring=score_params, return_train_score=False)
-    scores_lb = scores['test_accuracy'].mean()
-    k = 0
-    while (abs(ub-lb) >= 1) & (abs(scores_ub-scores_lb) >= 0.01):
-        if (scores_lb > scores_ub):
-            ub = round(abs(ub+lb)/2)
-            dummy = True
-            k = ub
-        else:
-            lb = round(abs(ub+lb)/2)
-            dummy = False
-            k = lb
-        if dummy == True:
-            print(ub)
-            classifier.set_params(n_neighbors = ub)
-            scores = cross_validate(classifier, dataset, y, scoring=score_params, return_train_score=False)
-            scores_ub = scores['test_accuracy'].mean()
-            print(scores_ub)
-        else:
-            print(lb)
-            classifier.set_params(n_neighbors = lb)
-            scores = cross_validate(classifier, dataset, y, scoring=score_params, return_train_score=False)
-            scores_lb = scores['test_accuracy'].mean()
-            print(scores_lb)
-
-
-    score_params = ['accuracy', 'f1_macro', 'f1_micro']
-    classifier.set_params(n_neighbors = k)
-    scores = cross_validate(classifier, dataset, y, scoring=score_params, return_train_score=False)
-
-    print(scores)
-
-    returnDf = pd.DataFrame(scores)
-    returnDf = returnDf.mean()
-
-    returnDf['Strategy'] = 'Binary Search'
-    returnDf['Dataset'] = 'D1'
-    returnDf['n_instances'] = n
-    returnDf['l_attributes'] = l
-    returnDf['k_neighbours'] = k
-
-    return returnDf
+        classifier.set_params(n_neighbors = ub)
+        scores = cross_validate(classifier, X, y, scoring=score_params, return_train_score=False)
+        scores_ub = scores['test_accuracy'].mean()
+        classifier.set_params(n_neighbors = lb)
+        scores = cross_validate(classifier, X, y, scoring=score_params, return_train_score=False)
+        scores_lb = scores['test_accuracy'].mean()
+        k = 0
+        while (abs(ub-lb) >= 1) & (abs(scores_ub-scores_lb) >= 0.01):
+            if (scores_lb > scores_ub):
+                ub = round(abs(ub+lb)/2)
+                dummy = True
+                k = ub
+            else:
+                lb = round(abs(ub+lb)/2)
+                dummy = False
+                k = lb
+            if dummy == True:
+                  print(ub)
+                  classifier.set_params(n_neighbors = ub)
+                  scores = cross_validate(classifier, X, y, scoring=score_params, return_train_score=False)
+                  scores_ub = scores['test_accuracy'].mean()
+                  print(scores_ub)
+                  score_params = ['accuracy', 'f1_macro', 'f1_micro']
+                  classifier.set_params(n_neighbors = k)
+                  scores = cross_validate(classifier, X, y, scoring=score_params, return_train_score=False)
+                  print(scores)
+                  scoreFrame = pd.DataFrame(scores)
+                  scoreFrame = scoreFrame.mean()
+                  returnDf = pd.DataFrame(scoreFrame)
+                  returnDf['Strategy'] = 'Binary Search'
+                  returnDf['Dataset'] = 'D' + str(inx)
+                  returnDf['n_instances'] = n
+                  returnDf['l_attributes'] = l
+                  returnDf['k_neighbours'] = k
+                  returnListDfs.append(returnDf)          
+            else:
+                print(lb)
+                classifier.set_params(n_neighbors = lb)
+                scores = cross_validate(classifier, X, y, scoring=score_params, return_train_score=False)
+                scores_lb = scores['test_accuracy'].mean()
+                print(scores_lb)
+                scoreFrame = pd.DataFrame(scores)
+                scoreFrame = scoreFrame.mean()
+                returnDf = pd.DataFrame(scoreFrame)
+                returnDf['Strategy'] = 'Binary Search'
+                returnDf['Dataset'] = 'D' + str(inx)
+                returnDf['n_instances'] = n
+                returnDf['l_attributes'] = l
+                returnDf['k_neighbours'] = k
+                returnListDfs.append(returnDf)
+    return returnListDfs
